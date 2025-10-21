a60d5ca068a0ffd9b72e19cb8a2d39763207da14
BmcHolding <gioia.bmc@gmail.com>
2025-10-21 20:57:08 +0200

    Refactor ReactDOM import and usage

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { HelmetProvider } from "react-helmet-async";
-import App from "./App.jsx";
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import { HelmetProvider } from 'react-helmet-async'
+import App from './App.jsx'
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <HelmetProvider>
       <App />
     </HelmetProvider>
   </React.StrictMode>
-);
+)
