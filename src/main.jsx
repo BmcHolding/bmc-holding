59acefea3b73d91b29702f6b894b3da2b751189a
BmcHolding <gioia.bmc@gmail.com>
2025-10-21 18:31:58 +0200

    Refactor main.jsx to include HelmetProvider

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,12 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { HelmetProvider } from "react-helmet-async";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(<App />)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <HelmetProvider>
+      <App />
+    </HelmetProvider>
+  </React.StrictMode>
+);
